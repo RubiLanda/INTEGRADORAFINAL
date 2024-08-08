576d8b6b409cd7e20b5a0c964dc464c1ba0eeda2
Jared Baldemar <baldelamar917@hotmail.com>
2024-08-08 08:47:33 -0600

    Parche de mostrar porductos

--- GESTION_AVANZADA_ADMINISTRADOR.sql
+++ GESTION_AVANZADA_ADMINISTRADOR.sql
@@ -714,53 +714,55 @@ create procedure Ver_Productos_Informacion(
     in p_records_per_page int
 )
 begin
-	if p_offset is null and p_records_per_page is null then
-		if p_categoria != 0 and p_nombre_producto is null then
-		
-			select id_producto as ID, nombre as Nombre, imagen as Imagen, precio as Precio, descripcion as Descripcion, estado as Estado
-			from PRODUCTOS   
-			where id_categoria = p_categoria;
-		end if;
-		
-		if p_categoria = 0 and p_nombre_producto is not null then
+if p_offset is null and p_records_per_page is null then
 
-			select id_producto as ID, nombre as Nombre, imagen as Imagen, precio as Precio, descripcion as Descripcion, estado as Estado
-			from PRODUCTOS
-			where  nombre = p_nombre_producto;
-		end if;
+if p_categoria != 0 and p_nombre_producto is null then
+
+select id_producto as ID, nombre as Nombre, imagen as Imagen, precio as Precio, descripcion as Descripcion, estado as Estado
+from PRODUCTOS   
+where id_categoria = p_categoria;
+end if;
 		
-		if p_categoria = 0 and p_nombre_producto is null then
+if p_categoria = 0 and p_nombre_producto is not null then
 
-			select id_producto as ID, nombre as Nombre, imagen as Imagen, precio as Precio, descripcion as Descripcion, estado as Estado
-			from PRODUCTOS;
-		end if;
-    else 
-		if p_categoria != 0 and p_nombre_producto is null then
-
-			select id_producto as ID, nombre as Nombre, imagen as Imagen, precio as Precio, descripcion as Descripcion, estado as Estado
-			from PRODUCTOS
-			where id_categoria = p_categoria
-			limit p_records_per_page
-			offset p_offset;
-		end if;
+select id_producto as ID, nombre as Nombre, imagen as Imagen, precio as Precio, descripcion as Descripcion, estado as Estado
+from PRODUCTOS
+where  nombre = p_nombre_producto;
+end if;
 		
-		if p_categoria = 0 and p_nombre_producto is not null then
+if p_categoria = 0 and p_nombre_producto is null then
 
-			select id_producto as ID, nombre as Nombre, imagen as Imagen, precio as Precio, descripcion as Descripcion, estado as Estado
-			from PRODUCTOS
-			where nombre = p_nombre_producto
-			limit p_records_per_page
-			offset p_offset;
-		end if;
+select id_producto as ID, nombre as Nombre, imagen as Imagen, precio as Precio, descripcion as Descripcion, estado as Estado
+from PRODUCTOS;
+end if;
+else 
+
+if p_categoria != 0 and p_nombre_producto is null then
+
+select id_producto as ID, nombre as Nombre, imagen as Imagen, precio as Precio, descripcion as Descripcion, estado as Estado
+from PRODUCTOS
+where id_categoria = p_categoria
+limit p_records_per_page
+offset p_offset;
+end if;
 		
-		if p_categoria = 0 and p_nombre_producto is null then
+if p_categoria = 0 and p_nombre_producto is not null then
 
-			select id_producto as ID, nombre as Nombre, imagen as Imagen, precio as Precio, descripcion as Descripcion, estado as Estado
-			from PRODUCTOS
-			limit p_records_per_page
-			offset p_offset;
-		end if;
-    end if;
+select id_producto as ID, nombre as Nombre, imagen as Imagen, precio as Precio, descripcion as Descripcion, estado as Estado
+from PRODUCTOS
+where nombre = p_nombre_producto
+limit p_records_per_page
+offset p_offset;
+end if;
+		
+if p_categoria = 0 and p_nombre_producto is null then
+
+select id_producto as ID, nombre as Nombre, imagen as Imagen, precio as Precio, descripcion as Descripcion, estado as Estado
+from PRODUCTOS
+limit p_records_per_page
+offset p_offset;
+end if;
+end if;
 end //
 DELIMITER ;
 
