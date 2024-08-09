5232459fd5e3558bd7c1a4409db3b74da7af9746
CrisMc00 <cristianmc1609@gmail.com>
2024-08-08 18:11:35 -0600

    .

--- php/nuevaimagen.php
+++ php/nuevaimagen.php
@@ -2,21 +2,21 @@
     include '../php/conexion.php';
     $Conexion = new Database();
     $Conexion->conectarBD();
+   $DireccionTemporal = $_FILES['imagen']['tmp_name'];
    $NombreArchivo = $_FILES['imagen']['name'];
-   $DireccionTemporal = '/var/www/html/img/temporal/'.$NombreArchivo;
    $Direccion = '/var/www/html/img/productos/';
    $DireccionConImagen = $Direccion.$NombreArchivo;
 
    $id = $_POST['id'];
-   $Imagenes = $Conexion->selectConsulta("SELECT * FROM PRODUCTOS WHERE PRODUCTOS.id_producto = $id");
-   $ImagenAntigua = $Imagenes[0]->imagen;
+   // $Imagenes = $Conexion->selectConsulta("SELECT * FROM PRODUCTOS WHERE PRODUCTOS.id_producto = $id");
+   // $ImagenAntigua = $Imagenes[0]->imagen;
 
-   if($ImagenAntigua && $ImagenAntigua !== $NombreArchivo){
-      $DireccionAntigua = $Direccion . $ImagenAntigua;
-      if (file_exists($DireccionAntigua)){
-         unlink($DireccionAntigua);
-      }
-   }
+   // if($ImagenAntigua && $ImagenAntigua !== $NombreArchivo){
+   //    $DireccionAntigua = $Direccion . $ImagenAntigua;
+   //    if (file_exists($DireccionAntigua)){
+   //       unlink($DireccionAntigua);
+   //    }
+   // }
 
    if (move_uploaded_file($DireccionTemporal,$DireccionConImagen)){
       $Conexion->ejecutar("CALL Modificar_Imagen_Producto($id,'$NombreArchivo',@mensaje)");
