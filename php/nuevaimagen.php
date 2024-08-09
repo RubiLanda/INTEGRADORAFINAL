5d8dd163059f764b2c86a5bc415169d8735fd8b2
Jared Baldemar <baldelamar917@hotmail.com>
2024-08-08 22:19:57 -0600

    de vuelta a la normalida?

--- php/nuevaimagen.php
+++ php/nuevaimagen.php
@@ -1,31 +1,27 @@
 <?php 
     include '../php/conexion.php';
-    $Conexion = new Database();
-    $Conexion->conectarBD();
+    $conexion = new Database();
+    $conexion->conectarBD();
    $DireccionTemporal = $_FILES['imagen']['tmp_name'];
    $NombreArchivo = $_FILES['imagen']['name'];
-   $Direccion = '/var/www/html/img/productos/';
+   $Direccion = '../img/productos/';
    $DireccionConImagen = $Direccion.$NombreArchivo;
 
    $id = $_POST['id'];
-   $Imagenes = $Conexion->selectConsulta("SELECT * FROM PRODUCTOS WHERE PRODUCTOS.id_producto = $id");
+   $Imagenes = $conexion->selectConsulta("SELECT * FROM PRODUCTOS WHERE id_producto = $id");
    $ImagenAntigua = $Imagenes[0]->imagen;
 
    if($ImagenAntigua && $ImagenAntigua !== $NombreArchivo){
       $DireccionAntigua = $Direccion . $ImagenAntigua;
       if (file_exists($DireccionAntigua)){
-         unlink($DireccionAntigua);
+       unlink($DireccionAntigua);
       }
    }
-
-      // Verifica si el directorio es escribible
-   if (!is_writable($uploadDir)) {
-      die('El directorio de subida no es escribible.');
-   }
+   
 
    if (move_uploaded_file($DireccionTemporal,$DireccionConImagen)){
-      $Conexion->ejecutar("CALL Modificar_Imagen_Producto($id,'$NombreArchivo',@mensaje)");
-      $consulta = $Conexion->selectConsulta("SELECT @mensaje as resultado");
+      $conexion->ejecutar("CALL Modificar_Imagen_Producto($id,'$NombreArchivo',@mensaje)");
+      $consulta = $conexion->selectConsulta("SELECT @mensaje as resultado");
       $mensaje = $consulta[0]->resultado;
       echo $mensaje;
    }
