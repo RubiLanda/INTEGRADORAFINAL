e7d23b80258019d8fa431884560e61cc5ac7c983
CrisMc00 <cristianmc1609@gmail.com>
2024-08-08 18:14:29 -0600

    .

--- php/nuevaimagen.php
+++ php/nuevaimagen.php
@@ -1,27 +1,47 @@
-<?php 
-    include '../php/conexion.php';
-    $Conexion = new Database();
-    $Conexion->conectarBD();
-   $DireccionTemporal = $_FILES['imagen']['tmp_name'];
-   $NombreArchivo = $_FILES['imagen']['name'];
-   $Direccion = '/var/www/html/img/productos/';
-   $DireccionConImagen = $Direccion.$NombreArchivo;
+<?php
+include '../php/conexion.php';
 
-   $id = $_POST['id'];
-   // $Imagenes = $Conexion->selectConsulta("SELECT * FROM PRODUCTOS WHERE PRODUCTOS.id_producto = $id");
-   // $ImagenAntigua = $Imagenes[0]->imagen;
+$Conexion = new Database();
+$Conexion->conectarBD();
 
-   // if($ImagenAntigua && $ImagenAntigua !== $NombreArchivo){
-   //    $DireccionAntigua = $Direccion . $ImagenAntigua;
-   //    if (file_exists($DireccionAntigua)){
-   //       unlink($DireccionAntigua);
-   //    }
-   // }
+$DireccionTemporal = $_FILES['imagen']['tmp_name'];
+$NombreArchivo = $_FILES['imagen']['name'];
+$Direccion = '/var/www/html/img/productos/';
+$DireccionConImagen = $Direccion . $NombreArchivo;
 
-   if (move_uploaded_file($DireccionTemporal,$DireccionConImagen)){
-      $Conexion->ejecutar("CALL Modificar_Imagen_Producto($id,'$NombreArchivo',@mensaje)");
-      $consulta = $Conexion->selectConsulta("SELECT @mensaje as resultado");
-      $mensaje = $consulta[0]->resultado;
-      echo $mensaje;
-   }
+// Obtener el ID de forma segura
+$id = (int)$_POST['id'];
+
+// Consultar la imagen existente usando consultas preparadas
+$Imagenes = $Conexion->selectConsulta("SELECT imagen FROM PRODUCTOS WHERE id_producto = ?", [$id]);
+if (!$Imagenes || !isset($Imagenes[0]->imagen)) {
+    die('No se encontró el producto o la imagen.');
+}
+
+$ImagenAntigua = $Imagenes[0]->imagen;
+
+if ($ImagenAntigua && $ImagenAntigua !== $NombreArchivo) {
+    $DireccionAntigua = $Direccion . $ImagenAntigua;
+    if (file_exists($DireccionAntigua)) {
+        if (!unlink($DireccionAntigua)) {
+            die('No se pudo eliminar la imagen antigua.');
+        }
+    }
+}
+
+if (move_uploaded_file($DireccionTemporal, $DireccionConImagen)) {
+    // Llamada segura a la consulta
+    $resultado = $Conexion->ejecutar("CALL Modificar_Imagen_Producto(?, ?, @mensaje)", [$id, $NombreArchivo]);
+    
+    // Obtener el mensaje
+    $consulta = $Conexion->selectConsulta("SELECT @mensaje as resultado");
+    if ($consulta && isset($consulta[0]->resultado)) {
+        $mensaje = $consulta[0]->resultado;
+        echo $mensaje;
+    } else {
+        echo 'No se pudo obtener el mensaje de resultado.';
+    }
+} else {
+    echo 'Error al mover el archivo.';
+}
 ?>
