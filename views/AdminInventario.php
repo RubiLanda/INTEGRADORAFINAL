b121e41619b85165e7e6420746bb5bba1d6b843b
Jared Baldemar <baldelamar917@hotmail.com>
2024-08-10 12:37:47 -0600

    correccion

--- views/AdminInventario.php
+++ views/AdminInventario.php
@@ -153,7 +153,6 @@ $menu2 = isset($_GET['estado']) ? false : true;
             </div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
         function ejecutarBoton(id_producto, id_input) {
