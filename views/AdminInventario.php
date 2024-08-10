234de0f633a87a953f89cd5527274bae03e48c29
Jared Baldemar <baldelamar917@hotmail.com>
2024-08-10 13:19:23 -0600

    barra desplegable

--- views/AdminInventario.php
+++ views/AdminInventario.php
@@ -188,8 +188,7 @@ $menu2 = isset($_GET['estado']) ? false : true;
         }
     </script>
     <script>
-      
-         const buttonMenu = document.getElementById('buttonMenu');
+      const buttonMenu = document.getElementById('buttonMenu');
         const menu = document.getElementById('menu');
         const buttonRegresar = document.getElementById('regresar');
 
@@ -199,14 +198,15 @@ $menu2 = isset($_GET['estado']) ? false : true;
 
         buttonRegresar.addEventListener('click', function() {
             menu.classList.add('oculto');
+            menu.classList.remove('mostrar')
         });
 
         document.addEventListener('click', function(event) {
             if (!menu.contains(event.target) && !buttonMenu.contains(event.target)) {
-                menu.classList.add('oculto');
+            menu.classList.remove('mostrar')
+            menu.classList.add('oculto');
             }
         });
-        alert(1);
     </script>
          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
