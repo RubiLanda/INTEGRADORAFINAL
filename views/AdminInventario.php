27498f0c33a6ee03f0f412f4f77f6e454c08c4df
Jared Baldemar <baldelamar917@hotmail.com>
2024-08-10 13:14:20 -0600

    .

--- views/AdminInventario.php
+++ views/AdminInventario.php
@@ -188,7 +188,7 @@ $menu2 = isset($_GET['estado']) ? false : true;
         }
     </script>
     <script>
-        alert(1);
+      
          const buttonMenu = document.getElementById('buttonMenu');
         const menu = document.getElementById('menu');
         const buttonRegresar = document.getElementById('regresar');
@@ -206,6 +206,7 @@ $menu2 = isset($_GET['estado']) ? false : true;
                 menu.classList.add('oculto');
             }
         });
+        alert(1);
     </script>
          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
