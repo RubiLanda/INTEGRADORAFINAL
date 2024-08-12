3d02d77753171f74217debf60c1b0c96716b9590
CrisMc00 <cristianmc1609@gmail.com>
2024-08-12 09:09:50 -0600

    .

--- views/RepartidorMicuenta.php
+++ views/RepartidorMicuenta.php
@@ -142,6 +142,7 @@ $menu2 = isset($_GET['estado']) ? false : true;
             }
         });
     </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
