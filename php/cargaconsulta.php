233497a1c6126aeb345c1908404c63a8759f669c
Jared Baldemar <baldelamar917@hotmail.com>
2024-08-12 11:43:07 -0600

    boton tipo reset

--- php/cargaconsulta.php
+++ php/cargaconsulta.php
@@ -50,12 +50,11 @@ foreach ($productos AS $reg)
                   echo "</button>";
             
                   // boton cancelar
-                  echo " <label for='BotonNoGuardar'>";      
+                  echo " <button id='BotonNoGuardar' type='reset' name='BotonNoGuardar'>";      
                   echo "<svg xmlns='http://www.w3.org/2000/svg' width='40' height='40' fill='red' class='bi bi-x-circle-fill' viewBox='0 0 16 16'>";
                   echo " <path d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z'/>";         
                   echo " </svg>";
-                  echo "</label>";        
-                  echo "<input id='BotonNoGuardar' type='reset' name='BotonNoGuardar'>";   
+                  echo "</button>";         
                   echo "</div>"; 
                   // div del input para habilitar y deshabilitar panes 
                   echo "<div class='div2 checkbox-contenedor'>";
