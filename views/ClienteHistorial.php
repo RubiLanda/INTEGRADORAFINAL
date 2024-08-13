16ed6a72c79e65837e19e6c35315e0cc1546929e
CrisMc00 <cristianmc1609@gmail.com>
2024-08-13 11:41:22 -0600

    .

--- views/ClienteHistorial.php
+++ views/ClienteHistorial.php
@@ -198,7 +198,7 @@ $usuario = $_SESSION['ID'];
 
 
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>l  
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> 
 </body>
 </html> 
 
