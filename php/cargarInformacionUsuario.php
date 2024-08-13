aae309f9cdbc38e0d270f94c01090d07513fc0ab
Jared Baldemar <baldelamar917@hotmail.com>
2024-08-13 17:01:07 -0600

    arreglo de botones

--- php/cargarInformacionUsuario.php
+++ php/cargarInformacionUsuario.php
@@ -36,17 +36,16 @@ foreach($reg as $r){
     <h3><b>Apellido Materno:</b><input type='text' name='materno' class='inputs' id='materno' value='{$r->AM_A}' oninput=Validarletras(this) minlength='3' maxlength='40'></h3>
     <h3><b>Telefono:</b> <input type='tel' name='telefono' id='telefono' class='inputs' value='{$r->T_A}' oninput=validarprecio(this) minlength='10' maxlength='10'> </h3>
     <div class='botoncat2'>
-    <button type='button' onclick=\"CambiarInfo($persona)\" >
+    <button type='button' onclick=\"CambiarInfo($usuario)\" >
     <svg xmlns='http://www.w3.org/2000/svg' width='40' height='40' fill='green' class='bi bi-check-circle-fill' viewBox='0 0 16 16'>
     <path d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z'/>
     </svg>
     </button>
-    <label for='BotonNoGuardar'>  
+     <button id='BotonNoGuardar' type='reset' name='BotonNoGuardar'>     
     <svg xmlns='http://www.w3.org/2000/svg' width='40' height='40' fill='red' class='bi bi-x-circle-fill' viewBox='0 0 16 16'>
-    <path d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z'/>     
-    </svg>
-            </label>      
-            <input id='BotonNoGuardar' type='reset' name='BotonNoGuardar'> 
+     <path d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z'/>        
+     </svg>
+    </button>
           </div>
           
           ";
