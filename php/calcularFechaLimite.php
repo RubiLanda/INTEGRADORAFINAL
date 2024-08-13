ebcddcf963dd4762870ddf98c4462522a78abd9d
CrisMc00 <cristianmc1609@gmail.com>
2024-08-13 13:29:23 -0600

    .

--- php/calcularFechaLimite.php
+++ php/calcularFechaLimite.php
@@ -5,10 +5,10 @@ $Conexion = new Database();
 $Conexion->conectarBD();
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
     $fecha = $_POST['fecha'];
-    // $fecha_limite = $Conexion->selectConsulta("call Calcular_Fecha_Limite('$fecha');");
-    // $resultado = $fecha_limite[0]->Fecha;
+    $fecha_limite = $Conexion->selectConsulta("call Calcular_Fecha_Limite('$fecha')");
+    $resultado = $fecha_limite[0]->Fecha;
 
-    echo $fecha;
+    echo $resultado;
 }
 
 ?>
