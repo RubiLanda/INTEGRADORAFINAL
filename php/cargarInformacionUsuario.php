6ccc3ab7bf1e2c1fd6a9c12f367574af5a592cd6
Jared Baldemar <baldelamar917@hotmail.com>
2024-08-13 17:36:17 -0600

    error cambiar info

--- php/cargarInformacionUsuario.php
+++ php/cargarInformacionUsuario.php
@@ -36,7 +36,7 @@ foreach($reg as $r){
     <h3><b>Apellido Materno:</b><input type='text' name='materno' class='inputs' id='materno' value='{$r->AM_A}' oninput=Validarletras(this) minlength='3' maxlength='40'></h3>
     <h3><b>Telefono:</b> <input type='tel' name='telefono' id='telefono' class='inputs' value='{$r->T_A}' oninput=validarprecio(this) minlength='10' maxlength='10'> </h3>
     <div class='botoncat2'>
-    <button type='button' onclick=\"CambiarInfo($usuario)\" >
+    <button type='button' onclick=\"CambiarInfo($persona)\" >
     <svg xmlns='http://www.w3.org/2000/svg' width='40' height='40' fill='green' class='bi bi-check-circle-fill' viewBox='0 0 16 16'>
     <path d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z'/>
     </svg>
