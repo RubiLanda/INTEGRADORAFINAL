39cc82b0e23195e3a48766497ff2689496a9adbb
CrisMc00 <cristianmc1609@gmail.com>
2024-08-13 10:55:04 -0600

    index responsiva

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
                     <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
                 </svg>
             </button>
-            <div>
+            <div class="login-registro">
                 <?php
                     session_start();
                     if (isset($_SESSION['Rol'])){
