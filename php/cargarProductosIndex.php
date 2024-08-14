1632c1a974715d8a2be92e26cf6d7b134e611ca6
CrisMc00 <cristianmc1609@gmail.com>
2024-08-14 09:04:10 -0600

    .

--- php/cargarProductosIndex.php
+++ php/cargarProductosIndex.php
@@ -10,22 +10,20 @@
 
     $offset = ($current_page - 1) * $records_per_page;
 
-    // $productos = $Conexion->selectConsulta("call Ver_Productos_Filtros($categoria_seleccionado, null, $offset, $records_per_page)");
+    $productos = $conexion->selectConsulta("call Ver_Productos_Filtros($categoria_seleccionado, null, $offset, $records_per_page)");
 
-    echo $offset;
+    foreach ($productos as $producto){
+    echo "<div class=\"Producto\">
+            <img src=\"../img/productos/{$producto->Imagen}\">
+            <div class=\"sombra\">
+                <h1>{$producto->Nombre}</h1>
+                <div>
+                    <h4>{$producto->Precio}</h4>
+                    <p>Ver descripcion</p>
+                </div>
+            </div>
+        </div>";
 
-    // foreach ($productos as $producto){
-    // echo "<div class=\"Producto\">
-    //         <img src=\"../img/productos/{$producto->Imagen}\">
-    //         <div class=\"sombra\">
-    //             <h1>{$producto->Nombre}</h1>
-    //             <div>
-    //                 <h4>{$producto->Precio}</h4>
-    //                 <p>Ver descripcion</p>
-    //             </div>
-    //         </div>
-    //     </div>";
-
-    // }
+    }
 ?>
     
