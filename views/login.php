f1420a0c93d452e80b584cebb24e1e9d2dc0e835
RubiLanda <rubivalenzuela8528@gmail.com>
2024-08-13 18:37:47 -0600

    ..

--- views/login.php
+++ views/login.php
@@ -26,14 +26,7 @@ if (isset($_SESSION['Rol']))
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <link rel="stylesheet" href="../css/registro.css">
     </head>
-    <body>
-    <div class="form">
-            <div class="divrefe">
-                <a href="../index.php"> <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="regresar" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"/>
-</svg>
-</a>
-        </div>
+
             <h2>Inicio de Sesión!</h2>
             
             <div class="inputs">
