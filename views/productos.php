3a5383dd613b42b8709ceb0046c96bfd0779290f
CrisMc00 <cristianmc1609@gmail.com>
2024-08-14 08:34:42 -0600

    .

--- views/productos.php
+++ views/productos.php
@@ -43,7 +43,7 @@ try {
                     <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
                 </svg>
             </button>
-            <div>
+            <div class="login-registro">
                 <?php
                     session_start();
                     if (isset($_SESSION['Rol'])){
