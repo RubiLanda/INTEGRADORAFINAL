65ff33126634aa814b30fde7b6c056ffed069ea5
RubiLanda <rubivalenzuela8528@gmail.com>
2024-08-14 13:54:49 -0600

    segjs

--- php/scriptventas.php
+++ php/scriptventas.php
@@ -5,7 +5,6 @@ $conexion->conectarBD();
 extract($_POST);
 // esto es de productos
 if ($meses=="REPORTE POR MES"){
-    
     $mes = "NULL";
 }
 else{
@@ -38,38 +37,49 @@ else{
 switch ($radiusActivo) {
     case 1:
         if ($fecha == "NULL"){
-            $consulta = $conexion->selectConsulta("call Ventas_Productos($mes,$año,$fecha,$categoria)"); //cuando es nulo con comillas y cuando no es nulo sin comillas
+            $consulta = $conexion->selectConsulta("call Ventas_Productos($mes,$año,$fecha,$categoria, @totalfinal)"); //cuando es nulo con comillas y cuando no es nulo sin comillas
         }else{
-            $consulta = $conexion->selectConsulta("call Ventas_Productos($mes,$año,'$fecha',$categoria)");
+            $consulta = $conexion->selectConsulta("call Ventas_Productos($mes,$año,'$fecha',$categoria,  @totalfinal)");
 
         }
+        $totalfinal=$conexion->selectConsulta("SELECT @totalfinal as TF");
         foreach($consulta as $fila){
             echo"<h3>{$fila->Producto}</h3>";
             echo"<h3>{$fila->Total}</h3>";
         }
+        echo"<h2>TOTAL FINAL</h2>";
+echo"<h2>\${$totalfinal[0]->TF}</h2>";
+
         break;
     case 2:
         if($fecha=="NULL"){
-            $consulta = $conexion->selectConsulta("call Ver_ventas_Tiendas($mes,$año, $fecha)");
+            $consulta = $conexion->selectConsulta("call Ver_ventas_Tiendas($mes,$año, $fecha, @totalfinal)");
         }else{
-            $consulta = $conexion->selectConsulta("call Ver_ventas_Tiendas($mes,$año,'$fecha')"); 
+            $consulta = $conexion->selectConsulta("call Ver_ventas_Tiendas($mes,$año,'$fecha',  @totalfinal)"); 
         }
+        $totalfinal=$conexion->selectConsulta("SELECT @totalfinal as TF");
         foreach($consulta as $fila){
             echo"<h3>{$fila->Nombre_tienda}</h3>";
             echo"<h3>{$fila->Total}</h3>";
         }
+        echo"<h2>TOTAL FINAL</h2>";
+echo"<h2>\${$totalfinal[0]->TF}</h2>";
+
 
         break;
     case 3:
         if($fecha=="NULL"){
-            $consulta = $conexion->selectConsulta("call Ver_Ventas_Repartidores($mes,$año,$fecha,$repa)");
+            $consulta = $conexion->selectConsulta("call Ver_Ventas_Repartidores($mes,$año,$fecha,$repa, @totalfinal)");
         }else{
-            $consulta = $conexion->selectConsulta("call Ver_Ventas_Repartidores($mes,$año,'$fecha',$repa)");
+            $consulta = $conexion->selectConsulta("call Ver_Ventas_Repartidores($mes,$año,'$fecha',$repa, @totalfinal)");
         }
+        $totalfinal=$conexion->selectConsulta("SELECT @totalfinal as TF");
         foreach($consulta as $fila){
             echo"<h3>{$fila->Nombre_repartidor}</h3>";
             echo"<h3>{$fila->Total}</h3>";
         }
+        echo"<h2>TOTAL FINAL</h2>";
+        echo"<h2>\${$totalfinal[0]->TF}</h2>";
         break;
 }
 ?>
