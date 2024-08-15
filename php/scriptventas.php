7b22f6c22a58898ce53badb7bd5d4b6f31c9f0e6
CrisMc00 <cristianmc1609@gmail.com>
2024-08-15 12:11:46 -0600

    .

--- php/scriptventas.php
+++ php/scriptventas.php
@@ -36,12 +36,7 @@ else{
 }
 switch ($radiusActivo) {
     case 1:
-        if ($fecha == "NULL"){
-            $consulta = $conexion->selectConsulta("call Ventas_Productos($mes,$año,$fecha,$categoria, @totalfinal)"); //cuando es nulo con comillas y cuando no es nulo sin comillas
-        }else{
-            $consulta = $conexion->selectConsulta("call Ventas_Productos($mes,$año,'$fecha',$categoria,  @totalfinal)");
-
-        }
+        $consulta = $conexion->selectConsulta("call Ventas_Productos($mes,$año,$fecha,$categoria, @totalfinal)"); //cuando es nulo con comillas y cuando no es nulo sin comillas
         $totalfinal=$conexion->selectConsulta("SELECT @totalfinal as TF");
         if(count($consulta)>0){
 
