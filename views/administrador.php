9b325e9caf1cba26d85cd9a5c0006e1e4ce5ee67
CrisMc00 <cristianmc1609@gmail.com>
2024-08-15 16:36:34 -0600

    .

--- views/administrador.php
+++ views/administrador.php
@@ -57,32 +57,53 @@ $menu2 = isset($_GET['estado']) ? false : true;
                 </svg>
             </button>
             <div> 
-                <div class='dropdown text-end'>
-                    <a href='#' class='d-block link-dark text-decoration-none perfil' id='dropdownUser1' data-bs-toggle='dropdown' aria-expanded='false'>
-                        <svg xmlns='http://www.w3.org/2000/svg' width='32' height='32' fill='currentColor' class='bi bi-person-fill' viewBox='0 0 16 16'>
-                            <path d='M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6'/>
-                        </svg>
-                    </a>
-                    <ul class='dropdown-menu text-small' aria-labelledby='dropdownUser1' data-popper-placement='bottom-end' style='position: absolute; inset: 0px auto auto 0px; margin: 0px; transform: translate(-112px, 34px);'>
-                        <li><a class='dropdown-item' href='#'>hola</a></li>
-                        <li><a class='dropdown-item' href='#'>Settings</a></li>
-                        <li><a class='dropdown-item' href='#'>Profile</a></li>
-                        <li><hr class='dropdown-divider'></li>
-                        <li><a class='dropdown-item' href='../php/cerrarSeccion.php'>Cerrar Sesión</a></li>
-                    </ul>
-                </div>
+            <?php
+                    if (isset($_SESSION['Rol'])){
+
+                        $cuenta = $conexion->selectConsulta("select USUARIOS.username as Nombre from USUARIOS where USUARIOS.id_usuario = '$persona'");
+
+                        echo "
+                        <div class='dropdown text-end'>
+                            <button id=\"carrito\" onclick=\"IrCarrito(1)\">
+                                <svg xmlns=\"http://www.w3.org/2000/svg\" width=\"50\" height=\"50\" fill=\"currentColor\" class=\"bi bi-cart4\" viewBox=\"0 0 16 16\">
+                                    <path d=\"M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5M3.14 5l.5 2H5V5zM6 5v2h2V5zm3 0v2h2V5zm3 0v2h1.36l.5-2zm1.11 3H12v2h.61zM11 8H9v2h2zM8 8H6v2h2zM5 8H3.89l.5 2H5zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0m9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0\"/>
+                                </svg>
+                            </button>
+                            <a href='#' class='d-block link-dark text-decoration-none perfil' id='dropdownUser1' data-bs-toggle='dropdown' aria-expanded='false'>
+                                <svg xmlns='http://www.w3.org/2000/svg' width='32' height='32' fill='currentColor' class='bi bi-person-fill' viewBox='0 0 16 16'>
+                                    <path d='M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6'/>
+                                </svg>
+                            </a>
+                            <ul class='dropdown-menu text-small' aria-labelledby='dropdownUser1' data-popper-placement='bottom-end' style='position: absolute; inset: 0px auto auto 0px; margin: 0px; transform: translate(-112px, 34px);'>
+                                <li><h6>hola,</h6></li>
+                                <li><h6>{$cuenta[0]->Nombre}</h6></li>
+                                <li><hr class='dropdown-divider'></li>";
+                            switch ($_SESSION['Rol']){
+                                case 1:
+                                    echo "<li><a class='dropdown-item' href='administrador.php'>Mi cuenta</a></li>";
+                                    break;
+                                case 2:
+                                    echo "<li><a class='dropdown-item' href='micuentacliente.php'>Mi cuenta</a></li>";
+                                    break;
+                                case 3:
+                                    echo "<li><a class='dropdown-item' href='RepartidorMicuenta.php'>Mi cuenta</a></li>";
+                                    break;
+                            }
+                            echo "<li><hr class='dropdown-divider'></li>
+                                <li><a class='dropdown-item' href='../php/cerrarSeccion.php'>Cerrar Sesión</a></li>
+                            </ul>
+                        </div>";
+                    }
+                    else {
+                        echo "<a href='views/login.php' class = 'boto'>Login</a>";
+                        echo "<a href='views/registro.php' class = 'boto'>Sign-up</a>";
+                    }
+                ?>
             </div>
         </div>
     </header>
 
-    <?php 
-    if ($menu1 && $menu2) {
-        echo "<div class=\"menu mostrar\" id=\"menu\">";
-    }
-    else {
-        echo "<div class=\"menu oculto\" id=\"menu\">";
-    }
-    ?>
+    <div class="menu oculto" id="menu"></div>
         <div class="inicioMenu">
             <img src="../img/logo.png">
             <button id="regresar" class="boton">
