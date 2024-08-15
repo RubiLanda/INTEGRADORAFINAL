f31691737cd1f2a5f80410045b2e2583bd6e02d5
CrisMc00 <cristianmc1609@gmail.com>
2024-08-15 15:02:45 -0600

    .

--- views/mispedidosrepa.php
+++ views/mispedidosrepa.php
@@ -58,8 +58,9 @@ else {
                                 </svg>
                             </a>
                             <ul class='dropdown-menu text-small' aria-labelledby='dropdownUser1' data-popper-placement='bottom-end' style='position: absolute; inset: 0px auto auto 0px; margin: 0px; transform: translate(-112px, 34px);'>
-                                <li>hola</li>
-                                <li>{$cuenta[0]->Nombre}</li>";
+                                <li><h6>hola,</h6></li>
+                                <li><h6>{$cuenta[0]->Nombre}</h6></li>
+                                <li><hr class='dropdown-divider'></li>";
                             switch ($_SESSION['Rol']){
                                 case 1:
                                     echo "<li><a class='dropdown-item' href='administrador.php'>Mi cuenta</a></li>";
