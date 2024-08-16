9350e788467c2878dab4292099e01af126f8962b
CrisMc00 <cristianmc1609@gmail.com>
2024-08-15 19:01:55 -0600

    .

--- php/VerAdmins.php
+++ php/VerAdmins.php
@@ -16,22 +16,22 @@ if ($_POST) {
             echo "<input type='checkbox' class='check' onclick=\"cambiarEstatus(this,{$fila->ID})\">";
         }
         echo "<button type=\"button\" class=\"boton\" data-bs-toggle=\"modal\" data-bs-target=\"#ModalInformacion{$fila->ID}\">
-    Ver Informacion
+    Ver Información
     </button>";
         echo "</div>";
         echo "<div class=\"modal fade\" id=\"ModalInformacion{$fila->ID}\" tabindex=\"-1\" aria-labelledby=\"exampleModalLabel\" aria-hidden=\"true\">
                     <div class=\"modal-dialog modal-dialog-centered\">
                         <div class=\"modal-content\">
                             <div class=\"modal-header\">
-                                <h1>Informacion</h1>
+                                <h1>Información</h1>
                                 <button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"modal\" aria-label=\"Close\"></button>
                             </div>
                             <div class=\"modal-body\">
                                 <h3><b>Usuario:</b>{$fila->Usuario}<h3>
                                 <h3><b>Nombre:</b>{$fila->Administrador}<h3>
                                 <h3><b>Fecha Nacimiento:</b>{$fila->Fecha_nacimiento}</h3>
-                                <h3><b>Genero:</b>{$fila->Genero}</h3>
-                                <h3><b>Telefono:</b>{$fila->Telefono}</h3>
+                                <h3><b>Género:</b>{$fila->Genero}</h3>
+                                <h3><b>Teléfono:</b>{$fila->Telefono}</h3>
                                 <h3><b>Fecha de Ingreso:</b>{$fila->Fecha_cargo}</h3>
                             </div>
                         </div>
