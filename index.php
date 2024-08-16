c6de0f61d20a1070c685e7c8675613311d059a50
CrisMc00 <cristianmc1609@gmail.com>
2024-08-16 00:55:08 -0600

    .

--- index.php
+++ index.php
@@ -107,7 +107,67 @@ $id = $_SESSION['ID'];
         <div></div>
     </div>
 
-    
+    <div class="ProductosDestacados">
+        <h1>Panes destacados</h1>
+        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <?php
+                $TopProductos = $Conexion->selectConsulta("select * from TopProductos");
+
+                for ($i=0; $i < count($TopProductos); $i++) { 
+                    if ($i == 0) {
+                        echo "<button type=\"button\" data-bs-target=\"#carouselExampleCaptions\" data-bs-slide-to=\"0\" class=\"active\" aria-current=\"true\" aria-label=\"Slide 1\"></button>";
+                    }
+                    else {
+                        echo "<button type=\"button\" data-bs-target=\"#carouselExampleCaptions\" data-bs-slide-to=\"{$i}\" aria-label=\"Slide {($i + 1)}\"></button>";
+                    }
+                }
+                
+                ?>
+            </div>
+            <div class="carousel-inner">
+                <?php
+                
+                for ($i=0; $i < count($TopProductos); $i++) { 
+                    if ($i == 0) {
+                        echo "
+                        <div class=\"carousel-item active\">
+                            <img src=\"img/productos/{$TopProductos[$i]->Imagen}\" class=\"d-block w-100\" id=\"sombra\">
+                            <div class=\"carousel-caption d-none d-md-block\">
+                                <h5>{$TopProductos[$i]->Nombre}</h5>
+                                <p>{$TopProductos[$i]->Precio}</p>
+                            </div>
+                        </div>
+                        ";
+                    }
+                    else {
+                        echo "
+                        <div class=\"carousel-item\">
+                            <img src=\"img/productos/{$TopProductos[$i]->Imagen}\" class=\"d-block w-100\" id=\"sombra\">
+                            <div class=\"carousel-caption d-none d-md-block\">
+                                <h5>{$TopProductos[$i]->Nombre}</h5>
+                                <p>{$TopProductos[$i]->Precio}</p>
+                            </div>
+                        </div>
+                        ";
+                    }
+                }
+                
+                ?>
+                
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>        
+    </div>
+
+    <div class="inbitacion">
 
     </div>
     
