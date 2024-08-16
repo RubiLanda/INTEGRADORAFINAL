1c257737346522ad59c1b594f4bce38164220f93
RubiLanda <rubivalenzuela8528@gmail.com>
2024-08-16 00:44:44 -0600

    .

---
+++ views/sobrenosotros.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/nosotros.css">
+    <title>¡Sobre Nosotros!</title>
+</head>
+<body>
+    <div class ="fondo"></div>
+    <div class="inicio">
+        <div>
+            <h1>¡Sobre Nosotros!</h1>
+            
+        </div>
+    </div>
+    <div class="container">
+        
+    <p>
+            <h3>HISTORIA</h3>    
+            <h4>Panadería La Espiga de Matamoros fundada en 1969 por el Sr. Raymundo González García en Matamoros Coahuila. Seguimos manteniendo su tradición ofreciendo un pan casero hecho a la leña mantenido ese sabor tan especial que nos ha caracterizado durante más de 50 años.</h4>
+        </p>
+        <div class="card">
+            <img src="../img/history.jpg"  class="card-img-top custom-img" alt="...">
+        </div>
+
+        <div class="row margen">
+            <div class="col-sm-12 col-lg-7">
+                <img src="../img/collage.jpg"  class="card-img-top custom-img" alt="...">
+            </div>
+            <div class="col-sm-12 col-lg-5">
+                <h3>Los mejores en calidad y sabor</h3>    
+                <h4>¡Pan casero, eso quiero!, panaderia La Espiga cuenta con los mejores sabores de la región</h4>
+            </div>
+        </div>
+
+        <div class="row margen">
+            <div class="col-12 text-center">
+                <h3>Visítanos</h3>    
+            </div>
+            <div class="col-12">
+                <iframe style="width:100%" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14400.863196657432!2d-103.22790241520258!3d25.531187681911483!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x868fc12a5a312709%3A0xfb270d2b4f37edff!2sAv%20Cuauht%C3%A9moc%20601-609%2C%20Vegas%20de%20Marrufo%20Oriente%2C%20Matamoros%2C%20Coah.!5e0!3m2!1ses-419!2smx!4v1723765448234!5m2!1ses-419!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>            
+            </div>
+        </div>
+
+
+
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
