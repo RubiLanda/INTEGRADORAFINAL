687afd97ac5f5fa17b6ab689979657f46098de0a
CrisMc00 <cristianmc1609@gmail.com>
2024-08-17 15:46:29 -0600

    .

--- views/micuentacliente.php
+++ views/micuentacliente.php
@@ -186,7 +186,46 @@ else {
         }
         
         function HABILITAR(checkbox, ID, Modal){
-            if (!checkbox.checkbox) {
+            if (checkbox.checkbox) {
+                var Estado;
+                if (checkbox.checked){
+                    Estado = 1;
+                }
+                else {
+                    Estado = 0;
+                }
+                $.ajax({
+                    type: 'POST',
+                    url: '../php/habiydesatienda.php',
+                    data: { ID: ID, Estado: Estado },
+                    success: function(response) {
+                        var toastContainer = document.getElementById('imprimirnoti');
+                        var newToast = document.createElement('div');  // Crear un nuevo elemento toast
+                        newToast.className = 'toast';
+                        newToast.setAttribute('role', 'alert');
+                        newToast.setAttribute('aria-live', 'assertive');
+                        newToast.setAttribute('aria-atomic', 'true');
+                        newToast.innerHTML = `  
+                        <div class="toast-header">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell-fill" viewBox="0 0 16 16">
+                        <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901"/>
+                        </svg><br>
+                        <strong class="me-auto"> NOTIFICACION </strong>
+                        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+                        </div>
+                        <div class="toast-body">
+                        ${response}
+                        </div>
+                        `;
+                        toastContainer.appendChild(newToast);  // Añadir el nuevo toast al contenedor
+                        var toast = new bootstrap.Toast(newToast, {  // Inicializar y mostrar el nuevo toast
+                            delay: 5000 // Duración del toast en milisegundos
+                        });
+                        toast.show();
+                    }
+                });
+            }
+            else {
                 $.ajax({
                     type: 'POST',
                     url: '../php/ContarPedidoTiendaPendientes.php',
@@ -234,45 +273,6 @@ else {
                                 }
                             });
                         }
-                    }
-                });
-            }
-            else {
-                var Estado;
-                if (checkbox.checked){
-                    Estado = 1;
-                }
-                else {
-                    Estado = 0;
-                }
-                $.ajax({
-                    type: 'POST',
-                    url: '../php/habiydesatienda.php',
-                    data: { ID: ID, Estado: Estado },
-                    success: function(response) {
-                        var toastContainer = document.getElementById('imprimirnoti');
-                        var newToast = document.createElement('div');  // Crear un nuevo elemento toast
-                        newToast.className = 'toast';
-                        newToast.setAttribute('role', 'alert');
-                        newToast.setAttribute('aria-live', 'assertive');
-                        newToast.setAttribute('aria-atomic', 'true');
-                        newToast.innerHTML = `  
-                        <div class="toast-header">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell-fill" viewBox="0 0 16 16">
-                        <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901"/>
-                        </svg><br>
-                        <strong class="me-auto"> NOTIFICACION </strong>
-                        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-                        </div>
-                        <div class="toast-body">
-                        ${response}
-                        </div>
-                        `;
-                        toastContainer.appendChild(newToast);  // Añadir el nuevo toast al contenedor
-                        var toast = new bootstrap.Toast(newToast, {  // Inicializar y mostrar el nuevo toast
-                            delay: 5000 // Duración del toast en milisegundos
-                        });
-                        toast.show();
                     }
                 });
             }
