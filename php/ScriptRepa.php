f04120cecbe490e565d3053d78a01b4e54e5f466
Ferydlc <mfdlcglz06@gmail.com>
2024-08-17 18:28:48 -0600

    repa ine

--- php/ScriptRepa.php
+++ php/ScriptRepa.php
@@ -37,6 +37,7 @@ foreach ($reg as $r) {
     <h3><b>Apellido Materno:</b><input type=\"text\" name=\"ApellidoM\" class=\"inputs\" id=\"ApellidoM\" value='{$r->AM_A}' oninput=Validarletras(this) minlength=\"3\" maxlength=\"50\"></h3>
     <h3><b>Teléfono:</b> <input type=\"tel\" name=\"TelefonoA\" id=\"TelefonoA\" class=\"inputs\" value='{$r->T_A}' oninput=validarprecio(this) minlength=\"10\" maxlength=\"10\"> </h3>
     <h3><b>Folio de conducir:</b> <input type=\"text\" name=\"FolCondu\" id=\"FolCondu\" class=\"inputs\" value='{$r->FL_R}' oninput=validarprecio(this) minlength=\"11\" maxlength=\"11\"> </h3>
+    <h3><label for='f{$r->id_user}'><img src='../infopersonal/{$r->INE}'></label><input id='f{$r->id_user}' type='file' onchange=\"modificarimagenine(this,{$r->id_user}\"></h3>
     <div class='botoncat'>
     <button type='button' onclick=\"CambiarInfo($persona)\" >
     <svg xmlns='http://www.w3.org/2000/svg' width='40' height='40' fill='green' class='bi bi-check-circle-fill' viewBox='0 0 16 16'>
