3ffd400e016ff54f7e601d861a4a4ed2c1ca93bd
CrisMc00 <cristianmc1609@gmail.com>
2024-08-18 14:44:22 -0600

    .

--- views/AdministradorVerPedidos.php
+++ views/AdministradorVerPedidos.php
@@ -183,7 +183,7 @@ $TipoCliente = isset($_GET['TipoCliente']) ? $_GET['TipoCliente'] : 1;
                 }
                 break;
         }
-        echo "<div>";
+        echo "<div class=\"OpcionesFiltro\">";
             echo "<input type=\"text\" id=\"\" placeholder=\"Buscar por ID\">";
             echo "<div>";
             echo "<h3>Por semana</h3>";
