24ef02bb00efca1261d4ed787c389f0b0d981ea2
Jared Baldemar <baldelamar917@hotmail.com>
2024-08-18 16:50:41 -0600

    .

--- views/RepartidorCargarVista.php
+++ views/RepartidorCargarVista.php
@@ -138,7 +138,8 @@ $menu2 = isset($_GET['estado']) ? false : true;
                       contentType: false,
                       processData: false,
                       success: function(response){
-                        
+                        alert(1);
+                        cargarInformacionUsuario()
   
                           var toastContainer = document.getElementById('toastContainer');
               var newToast = document.createElement('div');  // Crear un nuevo elemento toast
