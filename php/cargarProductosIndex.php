71433473023c42bbbac7afcd1f8b41d891ed7795
CrisMc00 <cristianmc1609@gmail.com>
2024-08-18 19:14:48 -0600

    .

--- php/cargarProductosIndex.php
+++ php/cargarProductosIndex.php
@@ -19,7 +19,22 @@
                 <h1>{$producto->Nombre}</h1>
                 <div>
                     <h4>\${$producto->Precio}</h4>
-                    <p>Ver descripción</p>
+                    <button type=\"button\" data-bs-toggle=\"modal\" data-bs-target=\"#ModalDescipcion{$producto->ID}\">
+                        Ver descripción
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div class=\"modal fade\" id=\"ModalDescipcion{$producto->ID}\" tabindex=\"-1\" aria-labelledby=\"exampleModalLabel\" aria-hidden=\"true\">
+            <div class=\"modal-dialog modal-dialog-centered\">
+                <div class=\"modal-content\">
+                <div class=\"modal-header\">
+                    <h5 class=\"modal-title\" id=\"exampleModalLabel\">{$producto->Nombre}</h5>
+                    <button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"modal\" aria-label=\"Close\"></button>
+                </div>
+                <div class=\"modal-body\">
+                    {$producto->Descripcion}
+                </div>
                 </div>
             </div>
         </div>";
